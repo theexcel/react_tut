9fce842ae8aef0066f1fa5b8ae73318872014d29
theexcel <xclolatunbosun@gmail.com>
2022-12-21 03:13:18 +0100

    react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect} from 'react';
+import axios from "axios";
+
 
 function App() {
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/comments").then((response) => {
+      console.log(response.data)
+    })
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+      
+        </div>
   );
 }
 
